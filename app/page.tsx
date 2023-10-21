1794f97b6539b9eb249d551669a90fef3139e157
Y.K <powdersugar828828@gmail.com>
2023-10-22 00:48:39 +0900

    feat: Sorted out of routing.

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,23 @@ export default function Home() {
     <ul>
       <li>
         <Link href="/sample" style={{ color: '#fff' }}>
-          sample
+          Client component
         </Link>
       </li>
       <li>
-        <Link href="/ssr-streaming" style={{ color: '#fff' }}>
-          ssr-streaming
-        </Link>
-      </li>
-      <li>
-        <Link href="/todos" style={{ color: '#fff' }}>
-          todos (server actions sample)
-        </Link>
-      </li>
-      <li>
-        <Link href="/todos/pattern2" style={{ color: '#fff' }}>
-          todos pattern2 (server actions sample)
-        </Link>
+        server actions sample
+        <ul style={{ marginLeft: '12px' }}>
+          <li>
+            <Link href="/todos" style={{ color: '#fff' }}>
+              todos（prisma直で実行してtodo取ってきてるパターン）
+            </Link>
+          </li>
+          <li>
+            <Link href="/todos/pattern2" style={{ color: '#fff' }}>
+              todos pattern2（SSR Streaming使ってtodo取ってきてるパターン）
+            </Link>
+          </li>
+        </ul>
       </li>
     </ul>
   );
