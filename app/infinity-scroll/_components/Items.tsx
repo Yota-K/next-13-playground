f4e1d0610e30dc5137e1f7ea0d573c22fc8a3add
Y.K <powdersugar828828@gmail.com>
2023-10-22 15:57:42 +0900

    feat: Implemented infinity scroll no library.

--- app/infinity-scroll/_components/Items.tsx
+++ app/infinity-scroll/_components/Items.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
+import { useCallback, useRef, useEffect, useState } from 'react';
 import { Box, Heading } from '@kuma-ui/core';
 
 type Item = {
@@ -14,36 +13,61 @@ type Props = {
 };
 
 export function Items({ initialItems, fetchItems }: Props) {
-  const fetching = useRef(false);
-  const [pages, setPages] = useState([initialItems]);
-  const items = pages.flatMap((page) => page);
+  const observerTarget = useRef(null);
 
-  const loadMore = async (page: number) => {
-    if (!fetching.current) {
-      try {
-        fetching.current = true;
+  const [items, setItems] = useState([initialItems]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(0);
 
-        const data = await fetchItems(page);
-        setPages((prev) => [...prev, data]);
-      } finally {
-        fetching.current = false;
-      }
+  const flatItems = items.flatMap((page) => page);
+
+  const loadMore = useCallback(
+    async (page: number) => {
+      const data = await fetchItems(page);
+      setItems((prev) => [...prev, data]);
+
+      const count = data.length;
+      setHasMore(count > 0);
+    },
+    [fetchItems]
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && hasMore) {
+            setPage((p) => p + 1);
+          }
+        });
+      },
+      { threshold: 1.0 }
+    );
+
+    let observerRefValue: null = null;
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+      observerRefValue = observerTarget.current;
     }
-  };
+
+    return () => {
+      if (observerRefValue) observer.unobserve(observerRefValue);
+    };
+  }, [hasMore, observerTarget]);
+
+  useEffect(() => {
+    if (page > 0) loadMore(page);
+  }, [page, loadMore]);
 
   return (
-    <InfiniteScroll
-      hasMore
-      pageStart={0}
-      loadMore={loadMore}
-      loader={<Heading my={20}>Loading ...</Heading>}
-      element="main"
-    >
-      {items.map((item) => (
+    <div>
+      {flatItems.map((item) => (
         <Box key={item.name} p={48} my={20} bg="white" color="black">
           {item.name}
         </Box>
       ))}
-    </InfiniteScroll>
+      <div ref={observerTarget}>{hasMore && <Heading my={20}>Loading ...</Heading>}</div>
+    </div>
   );
 }
